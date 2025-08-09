80ce8682465cc7c0930343eff4bc3e1492a156f6
MacBook Pro <macbookpro@sroo-punya.local>
2025-08-09 21:39:05 +0700

    add

--- src/lib/convert-image.ts
+++ src/lib/convert-image.ts
@@ -3,6 +3,7 @@ import sharp from "sharp";
 export const convertToWebP = async (file: File) => {
   const buffer = await file.arrayBuffer();
   const webpBuffer = await sharp(Buffer.from(buffer))
+    .rotate() // ini yang memperbaiki orientasi sesuai EXIF
     .resize({ width: 1280, withoutEnlargement: true })
     .webp({
       quality: 40,
